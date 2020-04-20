6a688ca1c1eaa368dedf30ee54439d972019d5a8
skakarla006 <skakarla006@gmail.com>
2020-04-20 18:34:20 -0500

    added shell script

---
+++ script.sh
@@ -0,0 +1,10 @@
+sudo yum update -y
+sudo yum install -y python3
+sudo yum install -y python-pip
+sudo pip3 install jupyterlab
+sudo pip3 install numpy
+sudo pip3 install pandas
+sudo pip3 install matplotlib
+sudo pip3 install seaborn
+sudo pip3 install sklearn
+nohup jupyter notebook &
